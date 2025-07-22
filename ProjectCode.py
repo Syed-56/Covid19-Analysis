9152f32a4a17dbc3c5d26feba183d1f30f7afb28
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-22 17:02:47 +0500

    Parsring URL Html

--- ProjectCode.py
+++ ProjectCode.py
@@ -1,5 +1,17 @@
+#Importing Libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 import requests
 from openpyxl import load_workbook
 from openpyxl.drawing.image import Image
+
+#Parsing Url
+url = "https://disease.sh/v3/covid-19/countries"
+response = requests.get(url)
+fileName = "covidData.html"
+
+if(response.status_code == 200):
+    with open(fileName, "w") as file:
+        file.write(response.text)   
+else:
+    print(f"Error {response.status_code}: Failed to retrieve data from the URL")
