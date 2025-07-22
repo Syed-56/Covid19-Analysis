80dbfd1200584e4537555a3ae747073d2a41d7a0
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-22 17:24:25 +0500

    Stored Data into JSON file

--- ProjectCode.py
+++ ProjectCode.py
@@ -2,22 +2,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import requests
-from bs4 import BeautifulSoup
+import json
 from openpyxl import load_workbook
 from openpyxl.drawing.image import Image
 
 #Parsing Url
 url = "https://disease.sh/v3/covid-19/countries"
 response = requests.get(url)
-fileName = "covidData.html"
+data = response.json()
+with open('Covid-Data.json', 'w') as f:
+    json.dump(data, f, indent=4)
 
-if(response.status_code == 200):
-    with open(fileName, "w") as file:
-        file.write(response.text)   
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-    print("Data fetched successfully and saved to", fileName)
-else:
-    print(f"Error {response.status_code}: Failed to retrieve data from the URL")
-
-print(soup.prettify()[:1000])  # Print first 1000 characters for brevity
+#Creating DataFrame
+# print(data)   Unformatted JSON data
+df = pd.json_normalize(data)
+print(df.head())
