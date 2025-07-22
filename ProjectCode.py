db84a0197e00bf9b6804515a25c1685d62e61ddf
Syed-56 <ssa.56.sultan@gmail.com>
2025-07-22 18:58:20 +0500

    Sorted Data with different parameters

--- ProjectCode.py
+++ ProjectCode.py
@@ -21,6 +21,19 @@ print(df.head())
 
 #Analyzing healthcare data
 df['death_rate'] = (df['deaths'] / df['cases']).round(4) * 100
-df['recovery_rate'] = (df['recovered'] / df['cases']).round(4) * 100
+df['recovery-rate'] = (df['recovered'] / df['cases']).round(4) * 100
 df['tests-per-case'] = (df['tests'] / df['cases']).round(2)
-print(df[['country', 'death_rate', 'recovery_rate', 'tests-per-case']].head())
+print(df[['country', 'death_rate', 'recovery-rate', 'tests-per-case']].head())
+
+#Sorting DataFrame
+mostAffected = df.sort_values(by='cases', ascending=False).head(10)
+leastAffected = df.sort_values(by='cases', ascending=True).head(10)
+highestDeathRate = df.sort_values(by='death_rate', ascending=False).head(10)
+lowestDeathRate = df.sort_values(by='death_rate', ascending=True).head(10)
+mostActiveCases = df.sort_values(by='active', ascending=False).head(10)
+
+print("Top 3 Most Affected Countries:\n", mostAffected[['country', 'cases']].head(3).to_string(index=False))
+print("Top 3 Least Affected Countries:\n", leastAffected[['country', 'cases']].head(3).to_string(index=False))
+print("Top 3 Countries with Highest Death Rate:\n", highestDeathRate[['country', 'death_rate']].head(3).to_string(index=False))
+print("Top 3 Countries with Lowest Death Rate:\n", lowestDeathRate[['country', 'death_rate']].head(3).to_string(index=False))
+print("Top 3 Countries with Most Active Cases:\n", mostActiveCases[['country', 'active']].head(3).to_string(index=False))
